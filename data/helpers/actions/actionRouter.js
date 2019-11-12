9f5fe3abb26d531f9774111a946494b1023642fe
Carmen <ccurry20@gmail.com>
2019-11-11 22:59:21 -0500

    Adding catch and validation

--- data/helpers/actions/actionRouter.js
+++ data/helpers/actions/actionRouter.js
@@ -7,39 +7,84 @@ const projectRouter = require("../projects/projectRouter.js");
 //router.use("/:id/projects", projectRouter);
 // router.use("/:id/actions", actionRouter);
 
-router.post("/", (req, res) => {
+router.post("/", validateActionId, validateActionPost, (req, res) => {
   const body = req.body;
-  actionModel.insert(body).then(action => {
-    res.status(201).json(action);
-  });
+  actionModel
+    .insert(body)
+    .then(action => {
+      res.status(201).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "internal server error" });
+    });
 });
 
-router.get("/", (req, res) => {
-  actionModel.get().then(actions => {
-    res.status(200).json(actions);
-  });
+router.get("/", validateActionId, validateActionPost, (req, res) => {
+  actionModel
+    .get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "internal server error" });
+    });
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", validateActionId, validateActionPost, (req, res) => {
   const id = req.params.id;
-  actionModel.get(id).then(action => {
-    res.json(action);
-  });
+  actionModel
+    .get(id)
+    .then(action => {
+      res.json(action);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "internal server error" });
+    });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", validateActionId, validateActionPost, (req, res) => {
   const id = req.params.id;
-  actionModel.remove(id).then(action => {
-    res.send("deleted");
-  });
+  actionModel
+    .remove(id)
+    .then(action => {
+      res.send("deleted");
+    })
+    .catch(err => {
+      res.status(500).json({ error: "internal server error" });
+    });
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", validateActionId, validateActionPost, (req, res) => {
   const id = req.params.id;
   const body = req.body;
-  actionModel.update(id, body).then(action => {
-    res.status(200).json(body);
-  });
+  actionModel
+    .update(id, body)
+    .then(action => {
+      res.status(200).json(body);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "internal server error" });
+    });
 });
+
+function validateActionId(req, res, next) {
+  const actionId = req.params.id;
+  actionModel.get(actionId).then(action => {
+    if (!action) {
+      res.status(400).json({ message: "action id not found" });
+    } else {
+      next();
+    }
+  });
+}
+
+function validateActionPost(req, res, next) {
+  const body = req.body;
+  if (!body.project.id || !body.description || !body.notes) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
--- data/helpers/projects/projectRouter.js
+++ data/helpers/projects/projectRouter.js
@@ -7,20 +7,30 @@ const projectRouter = require("../projects/projectRouter.js");
 // //router.use("/projects/:id", projectRouter);
 // router.use("/actions/id", actionRouter);
 
-router.post("/", (req, res) => {
+router.post("/", validateProjectId, validatePost, (req, res) => {
   const body = req.body;
-  projectModel.insert(body).then(project => {
-    res.status(201).json(project);
-  });
+  projectModel
+    .insert(body)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "internal server error" });
+    });
 });
 
-router.get("/", (req, res) => {
-  actionModel.get().then(projects => {
-    res.status(200).json(projects);
-  });
+router.get("/", validateProjectId, validatePost, (req, res) => {
+  actionModel
+    .get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "internal server error" });
+    });
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", validateProjectId, validatePost, (req, res) => {
   const id = req.params.id;
   projectModel
     .get(id)
@@ -39,26 +49,61 @@ router.get("/:id", (req, res) => {
 //   });
 // });
 
-router.get("/:id/actions", (req, res) => {
+router.get("/:id/actions", validateProjectId, validatePost, (req, res) => {
   const id = req.params.id;
-  projectModel.getProjectActions(id).then(project => {
-    res.json(project);
-  });
+  projectModel
+    .getProjectActions(id)
+    .then(project => {
+      res.json(project);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "internal server error" });
+    });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", validateProjectId, validatePost, (req, res) => {
   const id = req.params.id;
-  projectModel.remove(id).then(project => {
-    res.send("deleted");
-  });
+  projectModel
+    .remove(id)
+    .then(project => {
+      res.send("deleted");
+    })
+    .catch(err => {
+      res.status(500).json({ error: "internal server error" });
+    });
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", validateProjectId, validatePost, (req, res) => {
   const id = req.params.id;
   const body = req.body;
-  projectModel.update(id, body).then(project => {
-    res.status(200).json(body);
-  });
+  projectModel
+    .update(id, body)
+    .then(project => {
+      res.status(200).json(body);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "internal server error" });
+    });
 });
+
+function validateProjectId(req, res, next) {
+  const id = req.params.id;
+  projectModel.get(id).then(project => {
+    if (!project) {
+      res.status(400).json({ message: "project id not found" });
+    } else {
+      next();
+    }
+  });
+}
+
+function validatePost(req, res, next) {
+  const body = req.body;
+  if (!body.description || !body.name) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
